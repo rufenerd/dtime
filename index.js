f8adf383a149641eabd4932935f9dabadff094c6
Daniel Rufener <daniel.rufener@gmail.com>
2012-02-13 17:56:41 -0800

    convert both ways

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 function init(){
+  var onClick = function(){
+    var input = $('#inputField').val()
+    var split = input.split(":");
+    if($('input[value=oldToNew]').attr('checked')){
+      return oldToNew(new Date("1970-01-01 " + input));
+    } else {
+      return newToOld(input);
+    }
+  };
+
   $('#convertButton').click( function(){
-    $('#conversion').html(convertTime(new Date("2012-2-12 " + $('#oldTime').val())));
+    $('#conversion').html(onClick());
   });
   decTime();
 }
 
 function decTime() {
   var today = new Date();
-  var dtime = convertTime(today)
+  var dtime = oldToNew(today)
   document.getElementById('time').innerHTML = "<h1>"+dtime+"<h1>";
   setTimeout('decTime()');
 }
 
-function convertTime(time) {
+function oldToNew(time) {
   var h = time.getHours();
   var m = time.getMinutes();
   var s = time.getSeconds();
   var ms = time.getMilliseconds();
   var ms_today = 3600000*h + 60000*m + 1000*s + ms;
   return formatTime(Math.round(ms_today / 864))
+}
+
+function newToOld(time) {
+  var time = time.split(":")
+  var h = parseInt(time[0]);
+  var m = (time.length > 1) ? parseInt(time[1]) : 0;
+  var s = (time.length > 2) ? parseInt(time[2]) : 0;
+
+  var ms = (1000*h + 100*m + s) * 864;
+  console.log(h,m,s,ms, new Date(ms));
+  var dt = new Date(ms);
+  return dt.toGMTString().split(" ")[4];
 }
 
 function formatTime(time) {
